a812886d3f1d8674e0ace74996b2adff632b2a97
Jean Georges Perrin <jgp@jgp.net>
2017-05-31 16:28:44 -0400

    Added correct data types

--- src/main/java/net/jgp/labs/sparkdq4ml/dq/service/PriceCorrelationDataQualityService.java
+++ src/main/java/net/jgp/labs/sparkdq4ml/dq/service/PriceCorrelationDataQualityService.java
@@ -2,9 +2,9 @@ package net.jgp.labs.sparkdq4ml.dq.service;
 
 public abstract class PriceCorrelationDataQualityService {
 
-	public static Integer checkPriceRange(Integer t1, Integer t2) {
+	public static double checkPriceRange(double t1, double t2) {
 		// TODO Auto-generated method stub
-		return null;
+		return -1;
 	}
 
 }
--- src/main/java/net/jgp/labs/sparkdq4ml/dq/udf/PriceCorrelationDataQualityUdf.java
+++ src/main/java/net/jgp/labs/sparkdq4ml/dq/udf/PriceCorrelationDataQualityUdf.java
@@ -4,11 +4,11 @@ import org.apache.spark.sql.api.java.UDF2;
 
 import net.jgp.labs.sparkdq4ml.dq.service.PriceCorrelationDataQualityService;
 
-public class PriceCorrelationDataQualityUdf implements UDF2<Integer, Integer, Integer> {
+public class PriceCorrelationDataQualityUdf implements UDF2<Double, Integer, Double> {
 
 	private static final long serialVersionUID = 4949954702581973224L;
 
-	public Integer call(Integer t1, Integer t2) throws Exception {
+	public Double call(Double t1, Integer t2) throws Exception {
 		return PriceCorrelationDataQualityService.checkPriceRange(t1, t2);
 	}
 
