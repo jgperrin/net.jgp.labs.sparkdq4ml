95811569dac008a40ba2fa41399db27c48c0b134
Jean Georges Perrin <jperrin@lumeris.com>
2018-08-22 08:52:02 -0400

    Updated for Spark 2.3.1

--- src/main/java/net/jgp/labs/sparkdq4ml/DataQuality4MachineLearningApp.java
+++ src/main/java/net/jgp/labs/sparkdq4ml/DataQuality4MachineLearningApp.java
@@ -19,96 +19,108 @@ import net.jgp.labs.sparkdq4ml.ml.udf.VectorBuilder;
 
 public class DataQuality4MachineLearningApp {
 
-	public static void main(String[] args) {
-		DataQuality4MachineLearningApp app = new DataQuality4MachineLearningApp();
-		app.start();
-	}
+  public static void main(String[] args) {
+    DataQuality4MachineLearningApp app = new DataQuality4MachineLearningApp();
+    app.start();
+  }
 
-	private void start() {
-		SparkSession spark = SparkSession.builder().appName("DQ4ML").master("local").getOrCreate();
+  private void start() {
+    SparkSession spark = SparkSession.builder().appName("DQ4ML").master("local")
+        .getOrCreate();
 
-		// DQ Section
-		// ----------
-		
-		spark.udf().register("minimumPriceRule", new MinimumPriceDataQualityUdf(), DataTypes.DoubleType);
-		spark.udf().register("priceCorrelationRule", new PriceCorrelationDataQualityUdf(), DataTypes.DoubleType);
+    // DQ Section
+    // ----------
 
-		// Load our dataset
-		String filename = "data/dataset-abstract.csv";
-		Dataset<Row> df = spark.read().format("csv").option("inferSchema", "true").option("header", "false")
-				.load(filename);
-		
-		// simple renaming of the columns
-		df = df.withColumn("guest", df.col("_c0")).drop("_c0");
-		df = df.withColumn("price", df.col("_c1")).drop("_c1");
-		
-		System.out.println("----");
-		System.out.println("Load & Format");
-		df.show();
-		System.out.println("----");
-		
-		// apply DQ rules
-		// 1) min price
-		df = df.withColumn("price_no_min", callUDF("minimumPriceRule", df.col("price")));
-		System.out.println("----");
-		System.out.println("1st DQ rule");
-		df.printSchema();
-		df.show(50);
-		System.out.println("----");
-		
-		df.createOrReplaceTempView("price");
-		df = spark.sql("SELECT cast(guest as int) guest, price_no_min AS price FROM price WHERE price_no_min > 0");
-		System.out.println("----");
-		System.out.println("1st DQ rule - clean-up");
-		df.printSchema();
-		df.show(50);
-		System.out.println("----");
+    spark.udf().register("minimumPriceRule", new MinimumPriceDataQualityUdf(),
+        DataTypes.DoubleType);
+    spark.udf().register("priceCorrelationRule",
+        new PriceCorrelationDataQualityUdf(), DataTypes.DoubleType);
 
-		// 2) correlated price
-		df = df.withColumn("price_correct_correl", callUDF("priceCorrelationRule", df.col("price"), df.col("guest")));
-		df.createOrReplaceTempView("price");
-		df = spark.sql("SELECT guest, price_correct_correl AS price FROM price WHERE price_correct_correl > 0");
-		
-		df.show(50);
+    // Load our dataset
+    String filename = "data/dataset-abstract.csv";
+    Dataset<Row> df = spark.read().format("csv").option("inferSchema", "true")
+        .option("header", "false")
+        .load(filename);
 
-		// ML Section
-		// ----------
-		
-		spark.udf().register("vectorBuilder", new VectorBuilder(), new VectorUDT());
+    // simple renaming of the columns
+    df = df.withColumn("guest", df.col("_c0")).drop("_c0");
+    df = df.withColumn("price", df.col("_c1")).drop("_c1");
 
-		df = df.withColumn("label", df.col("price"));
-		df = df.withColumn("features", callUDF("vectorBuilder", df.col("guest")));
-		df.printSchema();
-		df.show();
+    System.out.println("----");
+    System.out.println("Load & Format");
+    df.show();
+    System.out.println("----");
 
-		// Lots of complex ML code goes here
-		
-		LinearRegression lr = new LinearRegression().setMaxIter(40);//.setRegParam(1).setElasticNetParam(1);
+    // apply DQ rules
+    // 1) min price
+    df = df.withColumn("price_no_min", callUDF("minimumPriceRule", df.col(
+        "price")));
+    System.out.println("----");
+    System.out.println("1st DQ rule");
+    df.printSchema();
+    df.show(50);
+    System.out.println("----");
 
-		// Fit the model to the data.
-		LinearRegressionModel model = lr.fit(df);
+    df.createOrReplaceTempView("price");
+    df = spark.sql(
+        "SELECT cast(guest as int) guest, price_no_min AS price FROM price WHERE price_no_min > 0");
+    System.out.println("----");
+    System.out.println("1st DQ rule - clean-up");
+    df.printSchema();
+    df.show(50);
+    System.out.println("----");
 
-		// Given a dataset, predict each point's label, and show the results.
-		model.transform(df).show();
+    // 2) correlated price
+    df = df.withColumn("price_correct_correl", callUDF("priceCorrelationRule",
+        df.col("price"), df.col("guest")));
+    df.createOrReplaceTempView("price");
+    df = spark.sql(
+        "SELECT guest, price_correct_correl AS price FROM price WHERE price_correct_correl > 0");
 
-		LinearRegressionTrainingSummary trainingSummary = model.summary();
-		System.out.println("numIterations: " + trainingSummary.totalIterations());
-		System.out.println("objectiveHistory: " + Vectors.dense(trainingSummary.objectiveHistory()));
-		trainingSummary.residuals().show();
-		System.out.println("RMSE: " + trainingSummary.rootMeanSquaredError());
-		System.out.println("r2: " + trainingSummary.r2());
+    df.show(50);
 
-		double intersect = model.intercept();
-		System.out.println("Interesection: " + intersect);
-		double regParam = model.getRegParam();
-		System.out.println("Regression parameter: " + regParam);
-		double tol = model.getTol();
-		System.out.println("Tol: " + tol);
-		Double feature = 40.0;
-		Vector features = Vectors.dense(feature);
-		double p = model.predict(features);
-		
-		System.out.println("Prediction for " + feature + " guests is " + p);
-		System.out.println(8 * regParam + intersect);
-	}
+    // ML Section
+    // ----------
+
+    spark.udf().register("vectorBuilder", new VectorBuilder(), new VectorUDT());
+
+    df = df.withColumn("label", df.col("price"));
+    df = df.withColumn("features", callUDF("vectorBuilder", df.col("guest")));
+    df.printSchema();
+    df.show();
+
+    // Lots of complex ML code goes here
+
+    LinearRegression lr = new LinearRegression()
+        .setMaxIter(40)
+        .setRegParam(1)
+        .setElasticNetParam(1);
+
+    // Fit the model to the data.
+    LinearRegressionModel model = lr.fit(df);
+
+    // Given a dataset, predict each point's label, and show the results.
+    model.transform(df).show();
+
+    LinearRegressionTrainingSummary trainingSummary = model.summary();
+    System.out.println("numIterations: " + trainingSummary.totalIterations());
+    System.out.println("objectiveHistory: " + Vectors.dense(trainingSummary
+        .objectiveHistory()));
+    trainingSummary.residuals().show();
+    System.out.println("RMSE: " + trainingSummary.rootMeanSquaredError());
+    System.out.println("r2: " + trainingSummary.r2());
+
+    double intersect = model.intercept();
+    System.out.println("Interesection: " + intersect);
+    double regParam = model.getRegParam();
+    System.out.println("Regression parameter: " + regParam);
+    double tol = model.getTol();
+    System.out.println("Tol: " + tol);
+    Double feature = 40.0;
+    Vector features = Vectors.dense(feature);
+    double p = model.predict(features);
+
+    System.out.println("Prediction for " + feature + " guests is " + p);
+    System.out.println(8 * regParam + intersect);
+  }
 }
