1a7db0dd72b805de8a91e5e941f30c4bde1737a9
jgp <jgp@jgp.net>
2019-10-15 10:02:11 -0400

    Format

--- src/main/java/net/jgp/labs/sparkdq4ml/dq/udf/MinimumPriceDataQualityUdf.java
+++ src/main/java/net/jgp/labs/sparkdq4ml/dq/udf/MinimumPriceDataQualityUdf.java
@@ -6,9 +6,9 @@ import net.jgp.labs.sparkdq4ml.dq.service.MinimumPriceDataQualityService;
 
 public class MinimumPriceDataQualityUdf implements UDF1<Double, Double> {
 
-	private static final long serialVersionUID = -201966159201746851L;
+  private static final long serialVersionUID = -201966159201746851L;
 
-	public Double call(Double t1) throws Exception {
-		return MinimumPriceDataQualityService.checkMinimumPrice(t1);
-	}
+  public Double call(Double t1) throws Exception {
+    return MinimumPriceDataQualityService.checkMinimumPrice(t1);
+  }
 }
